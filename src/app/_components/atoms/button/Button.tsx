1bda2713bdea2604ba4a3438b4b9200bd14bcdda
Glen Marti Diciano Ibias <mdiciano@snapp-solutions.com>
2026-03-09 10:32:48 +0100

    feat: add button component

---
+++ src/app/_components/atoms/button/Button.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { Slot } from "radix-ui";
+
+import { cn } from "@/lib/utils";
+
+import "./Button.css";
+
+const buttonVariants = cva("button", {
+  variants: {
+    variant: {
+      default: "button--default",
+      destructive: "button--destructive",
+      outline: "button--outline",
+      secondary: "button--secondary",
+      ghost: "button--ghost",
+      link: "button--link",
+    },
+    size: {
+      default: "button--size-default",
+      xs: "button--size-xs",
+      sm: "button--size-sm",
+      lg: "button--size-lg",
+      icon: "button--size-icon",
+      "icon-xs": "button--size-icon-xs",
+      "icon-sm": "button--size-icon-sm",
+      "icon-lg": "button--size-icon-lg",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+function Button({
+  className,
+  variant = "default",
+  size = "default",
+  asChild = false,
+  ...props
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot.Root : "button";
+
+  return (
+    <Comp
+      data-slot="button"
+      data-variant={variant}
+      data-size={size}
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+export { Button, buttonVariants };

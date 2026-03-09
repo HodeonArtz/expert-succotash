06a3799376879903edd3d89b6763501421be7d86
Glen Marti Diciano Ibias <mdiciano@snapp-solutions.com>
2026-03-09 10:33:04 +0100

    feat: add icon wrapper with iconmap

---
+++ src/app/_components/atoms/icon/Icon.tsx
@@ -0,0 +1,54 @@
+import {
+  IconCalendar,
+  IconCircleArrowRight,
+  IconGraph,
+  IconGrid3x3,
+  IconHelp,
+  IconLogout,
+  IconMail,
+  IconNotebook,
+  IconNotification,
+  IconPlayerPause,
+  IconPlayerPlay,
+  IconPlayerStop,
+  IconPlus,
+  IconSearch,
+  IconSettings,
+  IconUsers,
+  IconVideo,
+} from "@tabler/icons-react";
+import { SVGProps } from "react";
+
+// Nota: poner como nombre de icono un nombre descriptivo y semántico (para según qué contexto, qué es o qué va a hacer)
+export const iconsMap = {
+  grid: IconGrid3x3,
+  tasks: IconNotebook,
+  calendar: IconCalendar,
+  "analytic-graph": IconGraph,
+  team: IconUsers,
+  settings: IconSettings,
+  help: IconHelp,
+  logout: IconLogout,
+  search: IconSearch,
+  email: IconMail,
+  notification: IconNotification,
+  add: IconPlus,
+  navigate: IconCircleArrowRight,
+  record: IconVideo,
+  start: IconPlayerPlay,
+  pause: IconPlayerPause,
+  stop: IconPlayerStop,
+};
+
+export type IconName = keyof typeof iconsMap;
+
+interface Props extends SVGProps<SVGSVGElement> {
+  icon: IconName;
+}
+
+const Icon = ({ icon, ...props }: Props) => {
+  const IconElement = iconsMap[icon];
+  return <IconElement {...props} />;
+};
+
+export default Icon;
